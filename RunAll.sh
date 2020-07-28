e92e193ac6bec38321fcea5644496da631518d56
Matin Gühmann <guehmann@t-online.de>
2020-07-29 00:43:10 +0100

    Add RunAll bash scripts, one general, one for opsins, the genral is a stub for now

---
+++ RunAll.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get the directory where this script is
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+gene="$1"
+
+if [ -z "$gene" ]
+then
+	echo "You must give a GeneName, for instance:"
+	echo "./RunAll.sh GeneName"
+	exit
+fi
