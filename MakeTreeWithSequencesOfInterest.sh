abfecaa8194ea9531f92e7659bbd1ce7ace4b764
Matin Gühmann <guehmann@t-online.de>
2020-11-17 08:10:04 +0000

    Add code to extract from the tree of sequences of interest the sequnces themselves

--- MakeTreeWithSequencesOfInterest.sh
+++ MakeTreeWithSequencesOfInterest.sh
@@ -20,6 +20,8 @@ fi
 TreeForPruningDir="$DIR/$gene/TreeForPruning"
 TreeForPruning="$TreeForPruningDir/TreeForPruning.newick"
 TreeWithSequencesOfInterest="$TreeForPruningDir/TreeWithSequencesOfInterest.newick"
+SequencesOfInterest="$TreeForPruningDir/SequencesOfInterest.fasta"
+treeLabels="$TreeForPruningDir/LabelsOfInterest.txt"
 BaitDir="$DIR/$gene/BaitSequences/"
 AdditionalSequences="$DIR/$gene/AdditionalSequencesOfInterest/"
 
@@ -33,8 +35,6 @@ if [ -d $AdditionalSequences ]
 then
 	seqFiles+=($AdditionalSequences*.fasta)
 fi
-
-#echo ${seqFiles[@]}
 
 for seqFile in ${seqFiles[@]}
 do
@@ -57,3 +57,30 @@ do
 done
 
 "$DIR/../newick_utils/src/nw_clade" "$TreeForPruning" $LeavesOfSubTreeToKeep > $TreeWithSequencesOfInterest
+"$DIR/../newick_utils/src/nw_labels" $TreeWithSequencesOfInterest > $treeLabels
+
+sed -i "s/'//g" $treeLabels
+sed -i "s/\(^.*|.*|[^_]*_[^_]*\).*$/\1/g" $treeLabels
+sed -i "s/\(^[a-zA-Z]*_*[0-9]*\.[0-9]*\).*/\1/g" $treeLabels
+sed -i "s/\(^[a-zA-Z]*-[a-zA-Z0-9-]*\)_.*/\1/g" $treeLabels
+sed -i "s/\(^pdb|[a-zA-Z0-9-]*|[a-zA-Z0-9-]*\)_.*/\1/g" $treeLabels
+sed -i "s/\(^prf||[a-zA-Z0-9-]*\)_.*/\1/g" $treeLabels
+
+sequences="$DIR/$gene/Sequences"
+nrSequenceFile90="$sequences/NonRedundantSequences90.fasta"
+numTreads=$(nproc)
+
+seqkit grep -j $numTreads -f $treeLabels -t protein $nrSequenceFile90 > $SequencesOfInterest
+seqkit stats $SequencesOfInterest
+
+numExtractedSeqs=$(grep -c '>' $SequencesOfInterest)
+numExtractedLabels=$(grep -cve '^\s*$' $treeLabels)
+
+echo "$numExtractedLabels labels from subtree extracted."
+echo "$numExtractedSeqs sequences of interest extracted."
+if [[ $numExtractedSeqs == $numExtractedLabels ]]
+then
+	echo "No sequence where lost while extraction"
+else
+	echo "WARNING: Sequence lost during extraction, costum sequences might contain underscores that could not be detected."
+fi
