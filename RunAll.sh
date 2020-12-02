c25b72a6c46c899c5d1a805c98b1177b7e928b68
Matin Gühmann <guehmann@t-online.de>
2020-12-02 02:56:24 +0000

    Allow to run a range of the pipeline steps

--- RunAll.sh
+++ RunAll.sh
@@ -11,6 +11,7 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 gene="$1"
 step="$2"
+last="$3"
 
 if [ -z "$gene" ]
 then
@@ -26,63 +27,82 @@ then
 	exit
 fi
 
+
+# Note this must be set to the last available step
+lastStep="9"
+
+if [ -z "$last" ]
+then
+	last=$lastStep
+fi
+
+if [[ $last -gt $lastStep ]]
+then
+	last=$lastStep
+fi
+
 echo "Reconstruct phylogeny for $gene."
 echo ""
 
-case $step in
-0)
-	# Note if you want to rerun this step you must delete the files in \$gene\Hits\
-	echo "0. Obtaining gene IDs from all databases."
-	echo "   Searching for sequences in NCBI databases remotely, takes some time."
-	echo "   Therefore, just skip if files in $DIR/$gene/Hits/ already exist."
-	$DIR/GetGenesFromAllDataBases.sh $gene
-	echo "0. Gene IDs from all databases were obtained."
-	;&
-1)
-	echo "1. Combine the gene IDs for each database into one file, remove duplicates."
-	$DIR/CombineHitsForEachDatabase.sh $gene
-	echo "1. Gene IDs for each database were combined into one file, duplicates were removed."
-	;&
-2)
-	echo "2. Combine the gene IDs for each database into one file, remove duplicates."
-	$DIR/CombineHitsFromAllNCBIDatabases.sh $gene
-	echo "2. Gene IDs for each database were combined into one file, duplicates were removed."
-	;&
-3)
-	echo "3. Extract sequences from the databases."
-	$DIR/ExtractSequences.sh $gene
-	echo "3. Sequences from the database were extracted."
-	;&
-4)
-	echo "4. Make non redundant databases."
-	$DIR/MakeNonRedundant.sh $gene
-	echo "4. Non reduntant database were made."
-	;&
-5)
-	echo "5. Prepare sequences for CLANS."
-	$DIR/MakeClansFile.sh $gene
-	echo "5. Sequences have been prepared for CLANS."
-	;&
-6)
-	echo "6. Cluster sequences with CLANS."
-	$DIR/ClusterWithClans.sh $gene
-	echo "6. Sequences have been clustered with CLANS."
-	;&
-7)
-	echo "7. Create newick tree from CLANS file with neighbor joining for pruning."
-	$DIR/MakeTreeForPruning.sh $gene
-	echo "7. Newick tree from CLANS file with neighbor joining for pruning created."
-	;&
-8)
-	echo "8. Extract sequences of interest."
-	$DIR/ExtractSequencesOfInterest.sh $gene
-	echo "8. Sequences of interest extracted."
-	;&
-9)
-	echo "9. Align sequences with regressive T-Coffee."
-	$DIR/AlignWithTCoffee.sh $gene
-	echo "9. Sequences aligned with regressive T-Coffee."
-	;;
-*)
-	echo "Step $step is not a valid step."
-esac
+for ((i=$step;i<=last;i++))
+do
+	case $i in
+	0)
+		# Note if you want to rerun this step you must delete the files in \$gene\Hits\
+		echo "0. Obtaining gene IDs from all databases."
+		echo "   Searching for sequences in NCBI databases remotely, takes some time."
+		echo "   Therefore, just skip if files in $DIR/$gene/Hits/ already exist."
+		$DIR/GetGenesFromAllDataBases.sh $gene
+		echo "0. Gene IDs from all databases were obtained."
+		;;
+	1)
+		echo "1. Combine the gene IDs for each database into one file, remove duplicates."
+		$DIR/CombineHitsForEachDatabase.sh $gene
+		echo "1. Gene IDs for each database were combined into one file, duplicates were removed."
+		;;
+	2)
+		echo "2. Combine the gene IDs for each database into one file, remove duplicates."
+		$DIR/CombineHitsFromAllNCBIDatabases.sh $gene
+		echo "2. Gene IDs for each database were combined into one file, duplicates were removed."
+		;;
+	3)
+		echo "3. Extract sequences from the databases."
+		$DIR/ExtractSequences.sh $gene
+		echo "3. Sequences from the database were extracted."
+		;;
+	4)
+		echo "4. Make non redundant databases."
+		$DIR/MakeNonRedundant.sh $gene
+		echo "4. Non reduntant database were made."
+		;;
+	5)
+		echo "5. Prepare sequences for CLANS."
+		$DIR/MakeClansFile.sh $gene
+		echo "5. Sequences have been prepared for CLANS."
+		;;
+	6)
+		echo "6. Cluster sequences with CLANS."
+		$DIR/ClusterWithClans.sh $gene
+		echo "6. Sequences have been clustered with CLANS."
+		;;
+	7)
+		echo "7. Create newick tree from CLANS file with neighbor joining for pruning."
+		$DIR/MakeTreeForPruning.sh $gene
+		echo "7. Newick tree from CLANS file with neighbor joining for pruning created."
+		;;
+	8)
+		echo "8. Extract sequences of interest."
+		$DIR/ExtractSequencesOfInterest.sh $gene
+		echo "8. Sequences of interest extracted."
+		;;
+	9)
+		echo "9. Align sequences with regressive T-Coffee."
+		$DIR/AlignWithTCoffee.sh $gene
+		echo "9. Sequences aligned with regressive T-Coffee."
+		;;
+
+	# Adjust lastStep if you add more steps here
+	*)
+		echo "Step $i is not a valid step."
+	esac
+done
--- RunAllOpsins.sh
+++ RunAllOpsins.sh
@@ -9,4 +9,4 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-$DIR/RunAll.sh "Opsins" $1
+$DIR/RunAll.sh "Opsins" $1 $2
