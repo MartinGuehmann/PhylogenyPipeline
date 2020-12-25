4021f7c4bb48b8fd117781e1e2f30f2e14c96441
Matin Gühmann <guehmann@t-online.de>
2020-12-25 00:24:32 +0000

    Reduce the wall time for Building trees with IQ-Tree

--- PBS-Pro/10_PBS-Pro-MakeTreeWithIQ-Tree.sh
+++ PBS-Pro/10_PBS-Pro-MakeTreeWithIQ-Tree.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #PBS -l select=1:ncpus=24:mem=32gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=12:00:00
 
 # Go to the first program line,
 # any PBS directive below that is ignored.
