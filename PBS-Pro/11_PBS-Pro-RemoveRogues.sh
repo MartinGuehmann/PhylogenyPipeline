598f6c8673d7023660c521ef299ac80cb219bede
Martin Gühmann <guehmann@t-online.de>
2020-12-31 20:34:01 +0000

    Add also the Python version to be loaded

--- PBS-Pro/11_PBS-Pro-RemoveRogues.sh
+++ PBS-Pro/11_PBS-Pro-RemoveRogues.sh
@@ -6,7 +6,9 @@
 # Go to the first program line,
 # any PBS directive below that is ignored.
 # Modules to load, TreeShrink does not work with R >= 4.0
+# And we need Phython 2.7
 module load lang/r/3.6.1
+module load lang/python/anaconda/2.7-2019.10
 
 thisScript="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
