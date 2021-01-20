8819128506faff93d11b8ba11d6c9699123d0e06
Matin Gühmann <guehmann@t-online.de>
2021-01-20 04:56:01 +0000

    MUSCLE can only use one processor core

--- PBS-Pro/09_PBS-Pro-AlignWithMUSCLE.sh
+++ PBS-Pro/09_PBS-Pro-AlignWithMUSCLE.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#PBS -l select=1:ncpus=24:mem=100gb
-#PBS -l walltime=4:00:00
+#PBS -l select=1:ncpus=1:mem=32gb
+#PBS -l walltime=24:00:00
 
 # Go to the first program line,
 # any PBS directive below that is ignored.
