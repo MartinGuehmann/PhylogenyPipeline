9659d65ccb1e60d399b90f8e99133692ef40f113
Matin Gühmann <guehmann@t-online.de>
2021-02-01 22:16:14 +0000

    Add ClustalO as aligner

---
+++ 09_AlignWithClustalO.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Get the directory where this script is
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+# Directory and the name of this script
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+thisScript="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+
+# Input parameters
+gene="$1"                    # The gene name, the base directory for this analysis
+inputSequences="$2"          # The input sequences to be aligned
+alignmentDir="$3"            # The output directories for the alignments
+
+if [ -z "$gene" ]
+then
+	echo "You must give a GeneName, for instance:"
+	echo "./$thisScript GeneName InputSequences AlignmentDirectory"
+	exit
+fi
+
+if [[ -z "$inputSequences" ]]
+then
+	echo "You must give a file with InputSequences, for instance:"
+	echo "./$thisScript GeneName InputSequences AlignmentDirectory"
+	exit
+fi
+
+if [ -z "$alignmentDir" ]
+then
+	echo "You must give a file with InputSequences, for instance:"
+	echo "./$thisScript GeneName InputSequences AlignmentDirectory"
+	exit
+fi
+
+# Make input and output file names
+numTreads=$(nproc)
+base=$(basename $inputSequences .fasta)
+outFile="$alignmentDir/$base.alignment.ClustalO.fasta"
+outTree="$alignmentDir/$base.tree.ClustalO.newick"
+
+# Make alignment directory if it does not exist
+mkdir -p $alignmentDir
+
+# Align the sequences with ClustalO
+clustalo --threads "$numTreads" -i "$inputSequences" -o "$outFile" --guidetree-out="$outTree"
+
+###########################################################
+# Clean alignment of empty columns
+raxml-ng --msa "$outFile" --threads $numTreads --model LG+G --check
+
+# Remove double underscores and brackets from extended sequence IDs
+sed -i -e 's/__/_/g' -e 's/[][]//g' "$outFile.raxml.reduced.phy"
---
+++ PBS-Pro/09_PBS-Pro-AlignWithClustalO.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#PBS -l select=1:ncpus=24:mem=100gb
+#PBS -l walltime=8:00:00
+
+# Go to the first program line,
+# any PBS directive below that is ignored.
+# Load modules
+module load apps/clustal-omega/1.2.4-gcc
+
+thisScript="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+
+if [ -z "$gene" ]
+then
+	echo "You must give a GeneName, for instance:"
+	echo "./$thisScript GeneName"
+	exit
+fi
+
+date
+time "$DIR/../RunAll.sh" -g "$gene" -s "9" -l "9" -i "$iteration" -a "ClustalO" -f "$seqsToAlign"
+date
