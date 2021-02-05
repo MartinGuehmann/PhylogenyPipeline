c146fc8e876caab77f95c593b110c941ed93ba68
Matin Gühmann <guehmann@t-online.de>
2021-02-05 23:09:36 +0000

    Add PBS-Pro-RestoreSequenceOfInterest.sh for restoring SequencesOfInterest directories in batch mode

---
+++ PBS-Pro/PBS-Pro-RestoreSequenceOfInterest.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+# Get the directory where this script is
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+thisScript="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+
+# Idiomatic parameter and option handling in sh
+# Adapted from https://superuser.com/questions/186272/check-if-any-of-the-parameters-to-a-bash-script-match-a-string
+# And advanced version is here https://stackoverflow.com/questions/7069682/how-to-get-arguments-with-flags-in-bash/7069755#7069755
+while test $# -gt 0
+do
+    case "$1" in
+        --gene)
+            ;&
+        -g)
+            shift
+            gene="$1"
+            ;;
+        --iteration)
+            ;&
+        -i)
+            shift
+            iteration="$1"
+            ;;
+        --endIteration)
+            ;&
+        -e)
+            shift
+            endIteration="$1"
+            ;;
+        --aligner)
+            ;&
+        -a)
+            shift
+            aligner="$1"
+            ;;
+        --allSeqs)
+            ;&
+        -q)
+            allSeqs="--allSeqs"
+            ;;
+        --shuffleSeqs)
+            ;&
+        -l)
+            shuffleSeqs="--shuffleSeqs"
+            ;;
+        --suffix)
+            ;&
+        -x)
+            shift
+            suffix="-x $1"
+            ;;
+        --previousAligner)
+            ;&
+        -p)
+            shift
+            previousAligner="-p $1"
+            ;;
+        -*)
+            ;&
+        --*)
+            ;&
+        *)
+            echo "Bad option $1 is ignored"
+            ;;
+    esac
+    shift
+done
+
+if [ -z "$gene" ]
+then
+	echo "GeneName missing"
+	echo "You must give a GeneName and a StepNumber, for instance:"
+	echo "./$thisScript GeneName StepNumber"
+	exit
+fi
+
+if [ -z "$iteration" ]
+then
+	iteration="0"
+fi
+
+if [ -z "$endIteration" ]
+then
+	endIteration=$iteration
+fi
+
+if [ -z "$aligner" ]
+then
+	aligner=$("$DIR/../GetDefaultAligner.sh")
+fi
+
+jobIDs=""
+for ((i=$iteration;i<=endIteration;i++))
+do
+	jobIDs+=$($DIR/PBS-Pro-Call.sh             -g "$gene" -s "11" -i "$i" -a "$aligner" $allSeqs -d "$jobIDs" $shuffleSeqs $suffix $previousAligner --restore)
+done
+
+# Start held jobs
+jobIDs=$(echo $jobIDs | sed "s/:/ /g")
+echo $jobIDs
+qrls $jobIDs
