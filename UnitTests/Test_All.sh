65fdcd24d48dcda0315ece3b5b1551c25a6773ae
Matin Gühmann <guehmann@t-online.de>
2021-06-16 15:13:28 +0100

    Add Test_All.sh to UnitTests so that all test scripts can be called at once

---
+++ UnitTests/Test_All.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Get the directory where this script is
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+thisScript="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+
+
+# Run all bash test scrips in this directory
+for script in "$DIR/Test_"*".sh"
+do
+	if [ "$script" == "$DIR/$thisScript" ]
+	then
+		continue
+	fi
+
+	$script
+done
