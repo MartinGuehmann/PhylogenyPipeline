79af2f0ea46f05c67a63c1c678f405f6adf031a5
Matin Gühmann <guehmann@t-online.de>
2021-06-19 01:46:47 +0100

    Correct the wall time and meory requirements for steps 2 and 3

--- PBS-Pro/02_PBS-Pro-CombineHitsFromAllNCBIDatabases.sh
+++ PBS-Pro/02_PBS-Pro-CombineHitsFromAllNCBIDatabases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#PBS -l select=1:ncpus=24:mem=187gb
-#PBS -l walltime=72:00:00
+#PBS -l select=1:ncpus=8:mem=8gb
+#PBS -l walltime=4:00:00
 
 # Go to the first program line,
 # any PBS directive below that is ignored.
