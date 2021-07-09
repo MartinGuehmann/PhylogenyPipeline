4ee61c17d7a373ca72a2f5d6065eb07cbd847f15
Matin Gühmann <guehmann@t-online.de>
2021-07-09 09:15:25 +0100

    Add MAFFT-LINSI as an aligner

---
+++ PBS-Pro/09_PBS-Pro-AlignWithLINSI.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#PBS -l select=1:ncpus=24:mem=100gb
+#PBS -l walltime=24:00:00
+
+# Go to the first program line,
+# any PBS directive below that is ignored.
+# Load modules
+module load apps/mafft/7.429
+
+thisScript="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+
+if [ -z "$gene" ]
+then
+	echo "You must give a GeneName, for instance:" >&2
+	echo "./$thisScript GeneName" >&2
+	exit 1
+fi
+
+if [ ! -z $seqFiles ]
+then
+	seqsToAlign=$(cut -d " " -f ${PBS_ARRAY_INDEX} $seqFiles)
+fi
+
+date
+time "$DIR/../RunAll.sh" -g "$gene" -s "9" -i "$iteration" -a "LINSI" -f "$seqsToAlign" $suffix $previousAligner $trimAl
+date
