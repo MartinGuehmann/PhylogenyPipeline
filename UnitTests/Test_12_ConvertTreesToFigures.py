948458aea68aabd40747e69a4b0c81ab17b5035b
Matin Gühmann <guehmann@t-online.de>
2021-08-18 04:26:46 +0100

    Add unit test stub for 12_ConvertTreesToFigures.py

---
+++ UnitTests/Test_12_ConvertTreesToFigures.py
@@ -0,0 +1,12 @@
+# This is a stub for unit testing
+# Added in case this is ever implemented
+
+import unittest
+
+class Test_12_ConvertTreesToFigures(unittest.TestCase):
+	def test_1(self):
+#		../12_ConvertTreesToFigures.py
+		self.assertEqual(6, 6)
+
+if __name__ == '__main__':
+	unittest.main()
