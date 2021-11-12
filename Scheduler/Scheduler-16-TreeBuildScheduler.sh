42068a817716b495de7840576ea641f5551caee4
Matin Gühmann <guehmann@t-online.de>
2021-11-12 12:41:35 +0000

    Specify the needed ressources also in the scheduler scripts

--- Scheduler/Scheduler-16-TreeBuildScheduler.sh
+++ Scheduler/Scheduler-16-TreeBuildScheduler.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#PBS -l select=1:ncpus=1:mem=1gb
+#PBS -l walltime=0:10:00
+
+# Go to the first program line,
+# any PBS directive below that is ignored.
+# No modules to load
+
 if [ -z $DIR ]
 then
 	# Get the directory where this script is
