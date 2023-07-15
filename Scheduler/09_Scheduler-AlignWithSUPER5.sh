9fd6a281831756a2e22ac181a885ad8279a526ac
Matin Gühmann <guehmann@t-online.de>
2023-07-15 06:17:34 +0100

    Add the Super5 algorithm of Muscle5 as option

---
+++ Scheduler/09_Scheduler-AlignWithSUPER5.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#PBS -l select=1:ncpus=8:mem=32gb
+#PBS -l walltime=12:00:00
+
+# Go to the first program line,
+# any PBS directive below that, is ignored.
+# Load modules
+module load apps/muscle5/5.1
+
+thisScript="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+
+if [ -z "$gene" ]
+then
+	echo "You must give a GeneName, for instance:" >&2
+	echo "./$thisScript GeneName" >&2
+	exit 1
+fi
+
+if [ ! -z $seqFiles ]
+then
+	seqsToAlign=$(cut -d " " -f $("$DIR/Scheduler-GetArrayIndex.sh") $seqFiles)
+fi
+
+date
+time "$DIR/../RunAll.sh" -g "$gene" -s "9" -i "$iteration" -a "SUPER5" -f "$seqsToAlign" $suffix $previousAligner $trimAl
+date
